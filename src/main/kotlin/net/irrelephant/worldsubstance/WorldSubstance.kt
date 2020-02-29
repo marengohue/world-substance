b2532df191f2063caf10114232bff3fc4db1d90e
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2020-03-01 00:32:39 +0300

    Changed mod info and build props

---
+++ src/main/kotlin/net/irrelephant/worldsubstance/WorldSubstance.kt
@@ -0,0 +1,16 @@
+package net.irrelephant.worldsubstance
+
+// For support join https://discord.gg/v6v4pMv
+
+class WorldSubstance {
+
+    @Suppress("unused")
+    fun init() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        println("Hello Fabric world!")
+    }
+
+}
